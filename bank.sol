05d8afa02760db78d94a0a2e14ff9b2b98255e98
sixeyes-xyz <sixeyes.xyz@outlook.com>
2025-05-16 13:26:03 -0400

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Bank{
+contract bank{
   address public owner; // create address named owner
   
   constructor(){
@@ -9,11 +9,13 @@ contract Bank{
   }
   receive() external payable{} // allows for payments to be accepted
 
-  function withDraw() public{
+  function withdraw() public{
     require(owner == msg.sender, "You aren't the owner"); // requires so that the receiver is the owner
-    payable(owner).transfer(account(this).balance);  // transfers withdrawal
+    payable(owner).transfer(address(this).balance);  // transfers withdrawal; ERROR FIXED: from (account(this).balance) to (address(this).balance)
   }
   function balance() public view returns (uint){ // function to view balance, returning a uint
-      return (account(this).balance); // returns balance
+      return (address(this).balance); // returns balance
+
+//fixed some casing; withDraw to withdraw; Bank to bank
   }
 }
